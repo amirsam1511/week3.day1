7611e22ee32ca561f53d8e6043e452ba2e24d608
amirsam1511 <101941368+amirsam1511@users.noreply.github.com>
2022-04-02 20:01:24 +0530

    Create College.java

---
+++ Assignment2/College.java
@@ -0,0 +1,16 @@
+package org.college;
+
+//parent class
+public class College {
+	
+	public void collegeName() {
+		System.out.println("College Name is GCE,Salem");
+	}
+	public void collegeCode() {
+		System.out.println("College Code is 001");
+	}
+	public void collegeRank() {
+		System.out.println("College Rank is 10");
+	}
+
+}
