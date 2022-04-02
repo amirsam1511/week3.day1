14e8cc3c26fbdd28bbc79d7515e7d5ba966baf69
amirsam1511 <101941368+amirsam1511@users.noreply.github.com>
2022-04-02 23:42:45 +0530

    Update AxisBank.java

--- Assignment3/AxisBank.java
+++ Assignment3/AxisBank.java
@@ -1,10 +1,20 @@
 package week3.day1;
 
-public class AxisBank {
-	//parent Class
+public class AxisBank extends BankInfo{
+	//Child Class
 	
 	public void deposit() {
 		System.out.println("Deposit Amount in Axis Bank");
+	}
+	
+	public static void main(String[] args) {
+		//Creating object for Child class
+		AxisBank ab = new AxisBank();
+		//Method Overriding of deposit method in Axis Bank Class
+		ab.saving();
+		ab.fixed();
+		ab.deposit();
+
 	}
 
 }
